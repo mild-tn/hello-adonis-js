83326e9bbea908c64d00e7e99fa4f4a537ccb71c
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-11-13 00:33:08 +0700

    [Add] migrate competitor file

---
+++ database/migrations/1573577984443_competitor_schema.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class CompetitorSchema extends Schema {
+  up() {
+    this.create("competitors", table => {
+      table.increments("idCompetitor");
+      table.string("name");
+      table.string("nickname");
+      table.string("university");
+      table.string("like");
+      table.string("share");
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop("competitors");
+  }
+}
+
+module.exports = CompetitorSchema;
