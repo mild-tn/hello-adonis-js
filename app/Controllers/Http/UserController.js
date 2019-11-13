0ef68090e6c53b01359f2e1dd9cb0cc18e5a1d4f
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-11-13 16:29:42 +0700

    [Update] add vote

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,17 +1,33 @@
 "use strict";
 const User = use("App/Models/User");
+const Database = use("Database");
 
 class UserController {
   async createUser({ request, response }) {
     const data = request.raw();
     const competitor = JSON.parse(data);
     await User.create({
-      username: competitor.username,
-      email: competitor.email
+      username: competitor.username
     });
     return response.send({
       status: 200,
-      msg: "Created user !"
+      msg: "Created user !",
+      username: competitor.username
+    });
+  }
+
+  async voteCompetitors({ request, response }) {
+    const data = request.raw();
+    const competitor = JSON.parse(data);
+    const competitors = await Database.table("users")
+      .where("idCompetitor", "countOfVote")
+      .update({
+        idCompetitor: competitor.id,
+        countOfVote: competitor.count
+      });
+    return response.send({
+      status: 200,
+      msg: "updated !!"
     });
   }
 }
