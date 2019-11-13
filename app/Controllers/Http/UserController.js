2d9163c6dede41d578c0ae324df00b9c01c452d3
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-11-13 20:23:13 +0700

    [Update] sendtoken

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,6 +1,8 @@
 "use strict";
 const User = use("App/Models/User");
 const Database = use("Database");
+const Encryption = use("Encryption");
+const Hash = use("Hash");
 
 class UserController {
   async createUser({ request, response }) {
@@ -29,6 +31,33 @@ class UserController {
       status: 200,
       msg: "updated !!"
     });
+  }
+
+  async adminLogin({ request, response, auth }) {
+    console.log(auth);
+    const data = JSON.parse(request.raw());
+    const findUser = await Database.from("admins").where({
+      username: data.username
+    });
+    try {
+      const decryption = Encryption.decrypt(findUser[0].password);
+      if (
+        findUser[0].username === data.username &&
+        decryption === data.password
+      ) {
+        const token = await auth.generate(findUser[0]);
+        return response.send({
+          status: 200,
+          msg: "Username correct !!",
+          token: token
+        });
+      }
+    } catch (error) {
+      return response.send({
+        status: 401,
+        msg: "Username or Password incorrect !!"
+      });
+    }
   }
 }
 
