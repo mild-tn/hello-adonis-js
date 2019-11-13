93dbe7f9f34ca0b1b38f961e50a3397159b7940c
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-11-13 10:15:19 +0700

    [Update]  BodyParserProvider

--- start/app.js
+++ start/app.js
@@ -20,7 +20,8 @@ const providers = [
   "@adonisjs/session/providers/SessionProvider",
   "@adonisjs/auth/providers/AuthProvider",
   "@adonisjs/lucid/providers/LucidProvider",
-  "@adonisjs/shield/providers/ShieldProvider"
+  "@adonisjs/shield/providers/ShieldProvider",
+  "@adonisjs/bodyparser/providers/BodyParserProvider"
 ];
 
 /*
