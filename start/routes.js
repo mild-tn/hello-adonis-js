068eb16d7de6a7a7ae6f30d1f60bc84e1a3bc156
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-11-13 16:29:11 +0700

    [Update] edit routes.js

--- start/routes.js
+++ start/routes.js
@@ -18,3 +18,5 @@ const Route = use("Route");
 
 Route.on("/").render("welcome");
 Route.post("/loginusername", "UserController.createUser");
+Route.get("/competitor", "CompetitorController.getCompetitors");
+Route.post("/vote", "UserController.voteCompetitors");
