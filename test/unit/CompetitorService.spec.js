805221ecf3b35ed2ed2a71a3fa589c2a8ed25566
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-11-16 23:33:16 +0700

    [Update] add CompetitorService.spec.js

--- test/unit/CompetitorService.spec.js
+++ test/unit/CompetitorService.spec.js
@@ -1,10 +1,26 @@
 "use strict";
 
-const { test } = use("Test/Suite")("CompetitorService");
-const CompetitorCalculateScore = require("../../app/Service/CompetitorService");
+var assert = require("assert");
+var expect = require("chai").expect;
+var CompetitorService = require("../../app/Service/CompetitorService");
 
-test("should be return 6 when give [2,3,1]", async ({ assert }) => {
-  const countOfVote = [2, 3, 1];
-  const competitor = await CompetitorCalculateScore.calculateScore(countOfVote);
-  assert.equal(competitor, 6);
+describe("CompetitorService", () => {
+    describe("calculateScore()", async() => {
+        it("data of score it should be number", async() => {
+            const countOfVote = [2, 3, 1];
+            const score = await CompetitorService.calculateScore(countOfVote);
+            // expect(typeof score).to.be.an(number);
+            expect(score).to.be.an('number');
+        });
+        it('should be return "0" when give []', async() => {
+            const emptyVote = [];
+            const score = await CompetitorService.calculateScore(emptyVote);
+            expect(score).to.be.equal(0);
+        });
+        it("should be return 6 when give [2,3,1]", async() => {
+            const countOfVote = [2, 3, 1];
+            const score = await CompetitorService.calculateScore(countOfVote);
+            assert.equal(score, 6);
+        });
+    });
 });
